a20ee44817b20e882c973f67b544d6027d3f8800
Kigera Mark <kigeramark@gmail.com>
2024-10-06 23:39:07 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // Select the form and the tasks list
+  const form = document.getElementById("create-task-form");
+  const tasksList = document.getElementById("tasks");
+
+  // Add event listener to the form
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    const newTaskDescription = document.getElementById("new-task-description").value;
+
+    //New list item for the task
+    const newTask = document.createElement("li");
+    newTask.textContent = newTaskDescription;
+
+    // Delete button for the task
+    const deleteButton = document.createElement("button");
+    
+    deleteButton.addEventListener("click", function() {
+      tasksList.removeChild(newTask);
+      
+    });
+// Append the delete button to the task
+newTask.appendChild(deleteButton);
+
+// Append the new task to the tasks list
+tasksList.appendChild(newTask);
+
+// Clear the input field
+document.getElementById("new-task-description").value = "";
+    
+  });
 });
